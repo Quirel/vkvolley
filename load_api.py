7843aa2ba3f9eb9d1d085ee1268315b96ac612e9
Ilia Zhukov <quirel13@gmail.com>
2019-05-12 00:20:49 +1000

    fixed description and getting data from json

--- load_api.py
+++ load_api.py
@@ -3,17 +3,17 @@ import json
 import vk_api
 
 with open('vkauth.json', 'r') as f:
-    VK_KEYS = json.load(f)
+    VK_DATA = json.load(f)
 
 
-vk_session = vk_api.VkApi(token=VK_KEYS["VK_USER_ACCESS_TOKEN"])
+vk_session = vk_api.VkApi(token=VK_DATA["VK_USER_ACCESS_TOKEN"])
 vk = vk_session.get_api()
 
 # Get current user ID
 user_id = vk.users.get()[0]['id']
 
 # get wall of croup
-wall = vk.wall.get(owner_id=-182148977)
+wall = vk.wall.get(owner_id=VK_DATA['VK_GROUP_ID'])
 messages = wall['items']
 # get last not pinned message
 msg = [m for m in messages if not m.get('is_pinned', 0)][0]
