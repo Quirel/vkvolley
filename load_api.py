b80768951f73c9ccded6ea8958443328bc40b41a
Ilia Zhukov <quirel13@gmail.com>
2019-05-12 00:14:22 +1000

    base api functions works

--- load_api.py
+++ load_api.py
@@ -9,16 +9,14 @@ with open('vkauth.json', 'r') as f:
 vk_session = vk_api.VkApi(token=VK_KEYS["VK_USER_ACCESS_TOKEN"])
 vk = vk_session.get_api()
 
-# vk_session_service = vk_api.VkApi(app_id=int(VK_KEYS["VK_APP_ID"]), token=VK_KEYS["VK_APP_SERVICE"])
-# vk_service = vk_session_service.get_api()
-
 # Get current user ID
 user_id = vk.users.get()[0]['id']
 
-# wall = vk.wall.get(domain='volley19')
-# print(wall)
-
-# msg = vk_service.notifications.sendMessage(user_ids=user_id, message="test msg")
-# print(msg)
-
+# get wall of croup
+wall = vk.wall.get(owner_id=-182148977)
+messages = wall['items']
+# get last not pinned message
+msg = [m for m in messages if not m.get('is_pinned', 0)][0]
+# create comment on specified wall message
+# vk.wall.createComment(owner_id=msg['owner_id'], post_id=msg['id'], message='test comment')
 
