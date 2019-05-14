02a5ab69949715fc7564c629f338ddf19268fdef
Ilia Zhukov <quirel13@gmail.com>
2019-05-14 22:43:00 +1000

    Added basic Player model

--- volleyreg/mainapp/models.py
+++ volleyreg/mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    class Meta:
+        verbose_name = 'Игрок'
+        verbose_name_plural = 'Игроки'
+
+    name = models.CharField(verbose_name='Имя', max_length=128, unique=True)
+    register_4 = models.BooleanField(verbose_name='Запись на четверг', default=False)
+    register_6 = models.BooleanField(verbose_name='Запись на субботу', default=False)
+
+    def __str__(self):
+        return self.name
+
