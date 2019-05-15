23aa6b23bf21e6ff746ce7111910ee2920b356e1
Ilia Zhukov <quirel13@gmail.com>
2019-05-16 02:02:04 +1000

    Added Date field instead reg_4 and reg_6

--- volleyreg/mainapp/admin.py
+++ volleyreg/mainapp/admin.py
@@ -7,8 +7,8 @@ admin.site.site_header = 'VolleyReg administration'
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'registered', 'register_4', 'register_6']
+    list_display = ['id', 'name', 'date', 'registered']
     list_display_links = ['name']
-    list_editable = ['register_4', 'register_6']
+    list_editable = ['date']
     ordering = ('id',)
 
--- volleyreg/mainapp/models.py
+++ volleyreg/mainapp/models.py
@@ -7,9 +7,11 @@ class Player(models.Model):
         verbose_name_plural = 'Игроки'
 
     name = models.CharField(verbose_name='Имя', max_length=128, unique=True)
+    # TODO: remove old fields
     register_4 = models.BooleanField(verbose_name='Четверг', default=False)
     register_6 = models.BooleanField(verbose_name='Суббота', default=False)
-    registered = models.BooleanField(verbose_name='Зарегестрирован', default=False)
+    registered = models.BooleanField(verbose_name='ОК', default=False)
+    date = models.DateField(verbose_name="Дата", null=True)
 
     def __str__(self):
         return self.name
