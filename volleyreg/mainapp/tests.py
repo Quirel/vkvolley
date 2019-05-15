8043bbcae0ee1aeb3e1e3fe849fa30c46c4e0ed5
Ilia Zhukov <quirel13@gmail.com>
2019-05-16 02:22:05 +1000

    Modified Player model test

--- volleyreg/mainapp/tests.py
+++ volleyreg/mainapp/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+
 from django.test import TestCase
 
 from mainapp.models import Player
@@ -5,24 +7,11 @@ from mainapp.models import Player
 
 class PlayerTests(TestCase):
     def setUp(self):
-        Player.objects.create(name="Ilia", register_4=True)
-        Player.objects.create(name="Alexandra", register_6=True)
+        Player.objects.create(name="Ilia", date=dt.today().date())
 
     def test_name_content(self):
         player = Player.objects.get(id=1)
         self.assertEqual('Ilia', player.name)
-
-    def test_register_4_content(self):
-        player = Player.objects.get(id=1)
-        self.assertEqual(True, player.register_4)
-        player = Player.objects.get(id=2)
-        self.assertEqual(False, player.register_4)
-
-    def test_register_6_content(self):
-        player = Player.objects.get(id=1)
-        self.assertEqual(False, player.register_6)
-        player = Player.objects.get(id=2)
-        self.assertEqual(True, player.register_6)
 
     def test_get_registered(self):
         player = Player.objects.get(id=1)
