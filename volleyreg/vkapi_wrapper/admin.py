a879a65e3c6884bf3020fd5375cfcd40f136f67e
Ilia Zhukov <quirel13@gmail.com>
2019-05-16 16:44:47 +1000

    Admin for vk Keys

--- volleyreg/vkapi_wrapper/admin.py
+++ volleyreg/vkapi_wrapper/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from vkapi_wrapper.models import Keys
+
+
+@admin.register(Keys)
+class KeysAdmin(admin.ModelAdmin):
+    list_display = ['id', 'app_id', 'group_id']
+    list_display_links = ['id', 'app_id', 'group_id']
+    ordering = ('id',)
